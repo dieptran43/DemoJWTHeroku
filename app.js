540a5c9722e9c916dd3b343cdb56929404e4daba
dieptran43 <levandiep44@gmail.com>
2018-06-13 22:11:43 +0700

    Add mongo connection

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const app = express();
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
-const app = express();
-
 const log = console.log;
+
+const mongoose = require('mongoose');
+//Version3.6
+mongoose.connect(
+    "mongodb+srv://user_rest_shop:" +
+    process.env.MONGO_ATLAS_PW +
+    "@node-rest-shop-ricsq.mongodb.net/test?retryWrites=true"
+);
+
+//Version 3.4
+//mongodb://user_rest_shop:<PASSWORD>@node-rest-shop-shard-00-00-ricsq.mongodb.net:27017,node-rest-shop-shard-00-01-ricsq.mongodb.net:27017,node-rest-shop-shard-00-02-ricsq.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true
+// mongoose.connect(
+//     "mongodb://user_rest_shop:" +
+//     process.env.MONGO_ATLAS_PW +
+//     "@node-rest-shop-shard-00-00-ricsq.mongodb.net:27017,node-rest-shop-shard-00-01-ricsq.mongodb.net:27017,node-rest-shop-shard-00-02-ricsq.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true"
+// );
+mongoose.Promise = global.Promise;
 
 //s1 router
 const signinRoutes = require('./api/routes/signin');
