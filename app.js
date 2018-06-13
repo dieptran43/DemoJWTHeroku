907db08a3eb64ccd194a367b981e84b983fc2541
dieptran43 <levandiep44@gmail.com>
2018-06-13 20:54:35 +0700

    Update 04 request body and cors site

--- app.js
+++ app.js
@@ -7,21 +7,34 @@ const app = express();
 
 const log = console.log;
 
-app.use(morgan('dev')); //morgan to know url in server
-app.use('/', express.static('./public'));
-app.set('view engine', 'ejs');
-app.set('views','./views');
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use(cookieParser());
-
 //s1 router
 const signinRoutes = require('./api/routes/signin');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-app.get('/', (req, res, next)=>{
+app.use(morgan('dev')); //morgan to know url in server
+app.use('/', express.static('./public'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+app.get('/', (req, res, next) => {
     log('Vao day 1')
     res.status(200).render('index');
 });
@@ -31,20 +44,20 @@ app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
 
-// app.use((req, res, next) =>{
-//     let err = new Error('Not found page.');
-//     err.status = 404;
-//     next(err);
-// });
+app.use((req, res, next) => {
+    let err = new Error('Not found page.');
+    err.status = 404;
+    next(err);
+});
 
-// app.use((err, req, res, next)=>{
-//     res.status(err.status || 500);
-//     res.json({
-//         error:{
-//             message: err.message
-//         }
-//     });
-// });
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    });
+});
 //e1 router
 
 module.exports = app;
